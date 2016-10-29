6779875085584852d253f66d993f07dec9e838af
Hayden Smith <Haydenbsmith@gmail.com>
2016-10-29 18:58:06 +0100

    vid bg
    Added video bg

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'mlXJJoXW2qM', start: 1 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
