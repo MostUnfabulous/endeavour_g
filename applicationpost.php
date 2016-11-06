4a2f2c4de1bce8440fd6959f20bb0ee3584fdf25
Hayden Smith <Haydenbsmith@gmail.com>
2016-11-06 23:01:53 +0000

    Added Paul New Roster

--- applicationpost.php
+++ applicationpost.php
@@ -26,11 +26,11 @@ if(IsInjected($visitor_email))
     echo "Bad email value!";
     exit;
 }
-$to = "haydenbsmith@gmail.com,kinsula@gmail.com";//<== update the email address
+$to = "haydenbsmith@gmail.com,kinsula@gmail.com,pj930@hotmail.co.uk ";//<== update the email address
 $email_from = "Endeavour";//<== update the email address
 $email_subject = "$name Guild Application";
 $email_body =
-"New Guild application!\nQ1. Character Name: $name.\nQ2. Email adresss:$visitor_email.\n Q3. Armory Link: $link.\nQ4. Why join Endeavor? $join\nQ5.What is your experience? $expe\nQ6. Other information: $other";
+"New Guild application! \n Q1. Character Name: $name. \n Q2. Email adresss:$visitor_email. \n Q3. Armory Link: $link. \n Q4. Why join Endeavor? $join \n Q5.What is your experience? $expe \n Q6. Other information: $other";
 $headers = "From: $email_from \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
