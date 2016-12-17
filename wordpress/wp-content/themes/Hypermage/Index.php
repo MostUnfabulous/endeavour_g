96f55657912b6f7c277420ea383f9a1792efcc54
Hayden Smith <Haydenbsmith@gmail.com>
2016-12-17 00:47:52 +0000

    index footer removed

--- wordpress/wp-content/themes/Hypermage/Index.php
+++ wordpress/wp-content/themes/Hypermage/Index.php
@@ -91,9 +91,52 @@
       </div>
     </section>
 
+    <section id="three" class="wrapper style1 fade-up">
+      <div class="inner">
+        <h2>Apply Now!</h2>
+        <section>
+            <form method="post" name="application" action="applicationpost.php">
+              <div class="field half first">
+                <label for="name">Character Name</label>
+                <input type="text"  name="name" id="name" required >
+              </div>
+              <div class="field half">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" required />
+              </div>
+              <div class="field ">
+                <label for="arm"></br>Armoury Link</label>
+                <input type="text" name="link" id="link" required/>
+              </div>
+              <div class="field">
+                <label for="join"></br>Why do you want to join Endeavour?</label>
+                <textarea name="join" id="join" rows="3" required></textarea>
+                <div class="field">
+                  <label for="exp"></br>Tell us a bit about your raiding experience!</label>
+                  <textarea name="exp" id="exp" rows="3" required></textarea>
+                </div>
+                <div class="field">
+                  <label for="exp"></br>Anything else you'd like to add?</label>
+                  <textarea name="other" id="other" rows="3"></textarea>
+                </div>
+              </div>
+              <ul class="actions">
+                <li><input type="submit" name='submit' value="Send Application"></li>
+              </ul>
+            </form>
+          </section>
+          <section>
+      </div>
+    </section>
+    </div>
+
+
+    </div>
+    </section>
+
   <!-- Three -->
   <?php get_sidebar(); ?>
-<?php get_footer(); ?>
+
 
 
 <!-- Scripts -->
