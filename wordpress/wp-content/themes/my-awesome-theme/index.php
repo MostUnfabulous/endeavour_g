4a3c8d8fe29ae11babd561baf6670e84e4c78960
Hayden Smith <Haydenbsmith@gmail.com>
2016-12-17 01:57:34 +0000

    inxedheader

--- wordpress/wp-content/themes/my-awesome-theme/index.php
+++ wordpress/wp-content/themes/my-awesome-theme/index.php
@@ -5,17 +5,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>Endeavour-Magtheridon</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<link rel="stylesheet" href="assets\fonts\fontawesome\css\font-awesome.min.css">
-
-		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
-	</head>
+	get_header(); ?>
 	<body>
 
 		<!-- Sidebar -->
