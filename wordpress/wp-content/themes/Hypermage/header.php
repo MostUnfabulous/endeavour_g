f84e25326ef490a213fed735030b208f4220a35f
Hayden Smith <Haydenbsmith@gmail.com>
2016-12-17 00:42:35 +0000

    Hypermage basic

---
+++ wordpress/wp-content/themes/Hypermage/header.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Endeavour-Magtheridon</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="stylesheet" href="assets\fonts\fontawesome\css\font-awesome.min.css">
+
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+	</head>
---
+++ wordpress/wp-content/themes/Hypermage/sidebar.php
@@ -0,0 +1,14 @@
+<section id="sidebar">
+  <div class="inner">
+    <nav>
+      <ul>
+        <li><a href="#intro">Welcome</a></li>
+        <li><a href="#one">News</a></li>
+        <li><a href="#two">About Us</a></li>
+        <li><a href="#three">Apply Now!</a></li>
+      </ul>
+      </nav>
+    </div>
+
+
+</section>
