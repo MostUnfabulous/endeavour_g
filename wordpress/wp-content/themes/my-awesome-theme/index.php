e1bd5377a1e7542e090dd79e4680a03c63276b14
Hayden Smith <Haydenbsmith@gmail.com>
2016-12-17 02:00:17 +0000

    css header

--- wordpress/wp-content/themes/my-awesome-theme/index.php
+++ wordpress/wp-content/themes/my-awesome-theme/index.php
@@ -4,8 +4,8 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
-	get_header(); ?>
+
+	<?php get_header(); ?>
 	<body>
 
 		<!-- Sidebar -->
@@ -234,4 +234,3 @@
 			<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 	</body>
-</html>
