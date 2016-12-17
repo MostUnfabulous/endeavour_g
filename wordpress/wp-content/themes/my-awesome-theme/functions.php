c7d2c2f9c12668cec2238d83449b7d663c395dc6
Hayden Smith <Haydenbsmith@gmail.com>
2016-12-17 02:52:48 +0000

    test css

--- wordpress/wp-content/themes/my-awesome-theme/functions.php
+++ wordpress/wp-content/themes/my-awesome-theme/functions.php
@@ -10,8 +10,7 @@
  * First, let's set the maximum content width based on the theme's design and stylesheet.
  * This will limit the width of all uploaded images and embeds.
  */
-if ( ! isset( $content_width ) )
-    $content_width = 800; /* pixels */
+
 
 if ( ! function_exists( 'myfirsttheme_setup' ) ) :
 /**
@@ -23,35 +22,10 @@ if ( ! function_exists( 'myfirsttheme_setup' ) ) :
  */
 function myfirsttheme_setup() {
 
-    /**
-     * Make theme available for translation.
-     * Translations can be placed in the /languages/ directory.
-     */
-    load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
-
-    /**
-     * Add default posts and comments RSS feed links to <head>.
-     */
-    add_theme_support( 'automatic-feed-links' );
-
-    /**
-     * Enable support for post thumbnails and featured images.
+  
      */
     add_theme_support( 'post-thumbnails' );
 
-    /**
-     * Add support for two custom navigation menus.
-     */
-    register_nav_menus( array(
-        'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
-        'secondary' => __('Secondary Menu', 'myfirsttheme' )
-    ) );
-
-    /**
-     * Enable support for the following post formats:
-     * aside, gallery, quote, image, and video
-     */
-    
 }
 endif; // myfirsttheme_setup
 add_action( 'after_setup_theme', 'myfirsttheme_setup' );
