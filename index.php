7ea39625cb4e1fda7d2c89af4f7e7e8ae0a52a22
Hayden Smith <Haydenbsmith@gmail.com>
2017-01-31 06:35:52 +0000

    php

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<!--
+Hyperspace by HTML5 UP
+html5up.net | @ajlkn
+Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>Endeavour-Magtheridon</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	<link rel="stylesheet" href="assets\fonts\fontawesome\css\font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body>
+
+	<!-- Sidebar -->
+	<section id="sidebar">
+		<div class="inner">
+			<nav>
+				<ul>
+					<li><a href="#intro">Welcome</a></li>
+					<li><a href="#news">News</a></li>
+					<li><a href="#about">About Us</a></li>
+					<li><a href="#apply">Apply Now!</a></li>
+					<li><a href="roster.html">Roster</a></li>
+				</ul>
+			</nav>
+		</div>
+
+
+	</section>
+
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Intro -->
+		<section id="intro" class="wrapper style0 fullscreen fade-up">
+			<div class="inner">
+				<h1>ENDEAVOUR</h1>
+				<p>A chilled out semi-hardcore raiding guild on Magtheridon-EU.
+				</br>Currently recruiting awesome, like-minded players for Mythic progression.
+			</br>Scroll down or talk to one of us in-game for more info!</p>
+			<ul class="actions">
+				<li><a href="#news" class="button scrolly">Learn More</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- One -->
+	<section id="news" class="wrapper style2 spotlights">
+
+
+		<section>
+			<a href="xavmy.html" class="image"><img src="images/xavmy.jpg" alt="" data-position="top center" /></a>
+			<div class="content">
+				<div class="inner">
+					<h2>7/7 Mythic!</h2>
+					<h4>10th January</h4>
+					<p>Endeavour went into the Dream,</br>
+To end Xavey's nefarious scheme.</br>
+We fought dragon and bear,</br>
+And cleansed the Nightmare</br>
+Thanks to our awesome raid team!</p>
+					</div>
+				</div>
+			</section>
+
+		<section>
+			<a href="images/cenmy.jpg" class="image"><img src="images/cenmy.jpg" alt="" data-position="top center" /></a>
+			<div class="content">
+				<div class="inner">
+					<h2>Endeavour Bag the Stag</h2>
+					<h4>10th January</h4>
+					<p>
+Looks like we stepped up our game! After wiping fur a while, we upped our elk and raised the steaks - this evening,
+Endeavour bucked up the courage to grab Cenarius by the antlers and turn him into venison,
+before we high-tailed it out of there to face Xavius!</p>
+					</div>
+				</div>
+			</section>
+
+
+		<section>
+			<a href="images/ilgmy.jpg" class="image"><img src="images/ilgmy.jpg" alt="" data-position="top center" /></a>
+			<div class="content">
+				<div class="inner">
+					<h2>Endeavour See Eye-to-Eye with Il'gynoth</h2>
+					<h4>8th January</h4>
+					<p>What a spectacular sight to behold! Thanks to our fine and fervent focus, today Endeavour stared down the Evil Eye and obliterated
+						the oversized optical orifice - on only our fifth attempt! After a slippery skirmish of slime and steel,
+						we truncated its tremendous tentacles and eradicated the evil eyeball, slithering to a satisfying success.</p>
+					</div>
+				</div>
+			</section>
+
+		<section>
+			<a href="images/dragmy.jpg" class="image"><img src="images/dragmy.jpg" alt="" data-position="top center" /></a>
+			<div class="content">
+				<div class="inner">
+					<h2>A Tail of Triumph</h2>
+					<h4>18th December</h4>
+					<p>Today we tipped the scales in our favour and proved that we're up to scratch -
+						clawing our way into the Dreamway and achieving a
+						fangtastic victory as we took a bite out of the Dragons of Nightmare. It didn't drag on - we totally winged this one!</p>
+					</div>
+				</div>
+			</section>
+
+
+
+				<section>
+					<a href="images/ursmy.jpg" class="image"><img src="images/ursmy.jpg" alt="" data-position="top center" /></a>
+					<div class="content">
+						<div class="inner">
+							<h2>Endeavour Get Grizzly</h2>
+							<h4>30th November</h4>
+							<p>After some embearrassing wipes and an unbearable number of deaths, we finally got our bearings and bearied the
+								Great Bear Spirit with bearly a single raid member alive!
+								Thanks to our bear-faced efforts and bearbaric forbearance, we've made some bearilliant progress since our humble bearginnings.</p>
+								<ul class="actions">
+								</ul>
+							</div>
+						</div>
+					</section>
+
+
+
+
+						</section>
+
+						<!-- Two -->
+						<section id="about" class="wrapper style3 fade-up">
+							<div class="inner">
+								<h2>About Us</h2>
+								<p>Endeavour formed at the start of Legion, comprised mostly of veteran players - most of us have been raiding since Classic -
+									and since then, we've rapidly progressed through all raid difficulties. We achieved 7/7M on Jan 10th - killing Mythic Il'gynoth,
+									Cenarius and Xavius in just 3 days, thanks to the effort and perseverance of our awesome raid team. </p>
+
+<p>We offer a unique raiding experience - we're a very close-knit, chilled out guild, and while we're definitely serious
+ about progression, we want everyone to have fun and enjoy their time playing. Everyone is welcome (and encouraged!) to speak on Discord,
+ both in and out of raids. We value the input of EVERY player, and we want everyone to join in on the fun. </p>
+
+<p>For more information, check our the website. If you've got any questions, please don't hesitate to contact one of us in-game - Folez, Articon, Ereos and Femilian will be happy to talk.</p>
+									<div class="features">
+										<section>
+											<span class="icon major fa-calendar"></span>
+											<h3>Raid Schedule</h3>
+											<p>Progression raids are on Sunday, Monday and Wednesday, 8PM - 11PM Server time (7PM - 10PM UK time).
+												Raiders should be able to attend all three of these raids each week - but of course we understand that you'll sometimes have other committments.</p>
+											</section>
+											<section>
+												<span class="icon major fa-heart"></span>
+												<h3>Be Prepared!</h3>
+												<p>Raiders must bring a plentiful supply of potions, flasks and buff food to all progression runs. Your gear must be socketed
+													and enchanted with stats most appropriate to your spec.</P>
+												</section>
+												<section>
+													<span class="icon major fa-diamond"></span>
+													<h3>Rags to Riches</h3>
+													<p>We use a loot council to distribute loot in Mythic raids. Each week,
+														 we randomly assign two officers and two raiders to the loot council, and loot is allocated once a fair, unanimous decision is made.
+													</p>
+												</section>
+												<section>
+													<span class="icon major fa-bolt"></span>
+													<h3>Interface Addons</h3>
+													<p>We only ask that our raiders use the latest version of Deadly Boss Mods or BigWigs, Pawn and RCLootCouncil.
+														It's up to you if you want to use additional addons to help enhance your gameplay.</P>
+													</section>
+													<section>
+														<span class="icon major fa-users"></span>
+														<h3>Stay Awhile and Listen</h3>
+														<p>Everyone's expected to be on Discord during raids, but we encourage you to come on and chat
+															with us even when we aren't raiding!</p>
+															<a href="https://discord.gg/VmVDjva" class="button big" style="margin;">https://discord.gg/VmVDjva</a></p>
+														</section>
+														<section>
+															<span class="icon major fa-snowflake-o"></span>
+															<h3>Be Yourself!</h3>
+															<p>Be active, friendly and remember to have fun, and we're sure you'll
+																fit right in. Members start as a Trial, and we'll promote you to Raider after we test your worth in battle!</p>
+														</p><a href="roster.html" class="button big" style="margin-left:25%;">Guild Roster</a></p>
+													</section>
+												</div>
+											</div>
+										</section>
+
+										<!-- Three -->
+										<section id="apply" class="wrapper style1 fade-up">
+											<div class="inner">
+												<h2>Apply Now!</h2>
+												<section>
+													<form method="post" name="application" action="applicationpost.php">
+														<div class="field half first">
+															<label for="name">Character Name</label>
+															<input type="text"  name="name" id="name" required >
+														</div>
+														<div class="field half">
+															<label for="email">Email</label>
+															<input type="text" name="email" id="email" required />
+														</div>
+														<div class="field ">
+															<label for="arm"></br>Armoury Link</label>
+															<input type="text" name="link" id="link" required/>
+														</div>
+														<div class="field ">
+															<label for="class"></br>What is your class, spec, and current item level?</label>
+															<input type="text" name="class" id="class" required/>
+														</div>
+														<div class="field">
+															<label for="join"></br>Why do you want to join Endeavour?</label>
+															<textarea name="join" id="join" rows="3" required></textarea>
+															<div class="field">
+																<label for="exp"></br>Tell us a bit about your raiding experience!</label>
+																<textarea name="exp" id="exp" rows="3" required></textarea>
+															</div>
+															<div class="field">
+																<label for="exp"></br>Anything else you'd like to add?</label>
+																<textarea name="other" id="other" rows="3"></textarea>
+															</div>
+														</div>
+														<ul class="actions">
+															<li><input type="submit" name='submit' value="Send Application"></li>
+														</ul>
+													</form>
+												</section>
+							
+
+												</section>
+
+											<?php include("footer.php"); ?>
+
+								<!-- Scripts -->
+								<script src="assets/js/jquery.min.js"></script>
+								<script src="assets/js/jquery.scrollex.min.js"></script>
+								<script src="assets/js/jquery.scrolly.min.js"></script>
+								<script src="assets/js/skel.min.js"></script>
+								<script src="assets/js/util.js"></script>
+								<script src="assets/js/jquery.tubular.js"></script>
+								<script type="text/javascript" charset="utf-8" src="assets/js/index.js"></script>
+								<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+								<script src="assets/js/main.js"></script>
+								<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+							</body>
+							<?php include("footer.php"); ?>
+							</html>
