c6cbb508c54dc5091ec3622297806aa7e7d4c0cc
Hayden Smith <Haydenbsmith@gmail.com>
2017-02-12 17:46:16 +0000

    Charter

--- charter.php
+++ charter.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>7/7 Mythic!</title>
+		<title>Charter</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -21,7 +21,7 @@
 				<a href="index.html" class="title">Endeavour</a>
 				<nav>
 					<ul>
-						<li><a href="index.html">Home</a></li>
+						<li><a href="index.php">Home</a></li>
 					</ul>
 				</nav>
 			</header>
@@ -34,23 +34,24 @@
 						<div class="inner">
 							<h1 class="major">Charter</h1>
 							<h2>Our Vision</h2>
-								<p>We are a small but tightly-knit community of raiders who are dedicated not neccesserily to expending large amounts of time on completing content, but instead to make the most out of the little time we have allocated for progression content. As a result, we are very relaxed outside of raids: our community is filled with chilled and incredibly funny people. In raids however, we aim to complete those with the full dedication of raiders, consistency and seriousness, so that we can reach our goal of completing content at the highest level without raiders having to attend extreme amounts of raids every week.</p>
+								<p>We are a <b>small but tightly-knit community</b> of raiders who are dedicated not necessarily to expending large amounts of time on completing content, but instead to make the most out of the little time we have allocated for progression content. As a result, we are very relaxed outside of raids: our community is filled with chilled and incredibly funny people.
+									In raids however, we aim to complete those with the <b>full dedication of raiders</b>, consistency and seriousness, so that we can reach our goal of completing content at the highest level without raiders having to attend extreme amounts of raids every week.</p>
 							<h2>Attendance</h2>
-							<p>Our progression and farm raids are on Wednesdays, Sundays and Mondays at 8-11pm.
-								We will run slightly less relevant content on Thursday nights – those raids are optional. Raiders will be expected to attend the three main
-								raids every week, although we understand that you will sometimes have inevitable commitments – inform an officer of the reason.
+							<p>Our progression and farm raids are on <b>Wednesdays, Sundays and Mondays at 8-11pm.</b>
+								We will run slightly less relevant content on Thursday nights – those raids are optional.<b>Raiders will be expected to attend the three main
+								raids every week</b>, although we understand that you will sometimes have inevitable commitments – inform an officer of the reason.
 								Aim for a 95%+ attendance whenever possible.</p>
 								<h2>Roster</h2>
 							<p>We plan to have a main roster of about 24 people, and in the event of having a surplus of players for a mythic progression raid,
-								we will prioritize firstly those who signed up on the calendar, and then those who
+								we will prioritize firstly those <b>who signed up on the calendar</b>, and then those who
 								 are the most relevant for the fight. Please do not feel offended or disappointed about being benched –
-								 it usually is not due to your personal performance and you will get your chance soon. On farm runs we prioritize those who requires gear. </p>
+								 it usually is not due to your personal performance and you will get your chance soon. On farm runs we <b>prioritize those who requires gear.</b></p>
 								 <h2>Expectations in raids and outside of raids</h2>
 									 <p>There is a <b>minimum</b> expectation that you will bring relevant flasks, potions and food before you turn
 										 up at a raid. It is also recommended that you bring augment runes,
 										  although you will not be asked to use it very often. We have members who have the relevant profession to craft consumables –
 										 simply ask them nicely before the raid. In addition, it goes without saying that your gear must be fully enchanted & gemmed.
-										 In addition, you will be required to have installed the addons DBM/BigWigs, RCLootCouncil and BestInSlot. We also highly recommend WeakAuras and Pawn, although those require setting up. </p>
+										 In addition, you will be required to have installed the addons DBM/BigWigs, RCLootCouncil and BestInSlot. We also highly recommend WeakAuras and Pawn, although those require setting up.</p>
 										 <p>You must be fully committed to the raid after we begin, since we only have three days per week to complete progression content
 											  (which means minimal AFK, especially on trash pulls, and using consumables, including pre-potting).
 												 You must be in the Discord server and can listen - working microphones are not required, but are <b>highly recommended.</b>
@@ -62,10 +63,10 @@
 													we do want you to have fun so have liberty in this,
 												 although we will inform you if we feel that you are not putting enough effort into your characters.</p>
 												 <h2>Administration & Complaints</h2>
-													 <p>Almost all loot from progression and farm raids will be distributed using a loot council. Each week, two randomly assigned officers + raiders will make up the council and loot is allocated once a fair, unanimous decision is made. Socials, trials and new raiders (those who've been promoted to raid in the past 2+ weeks) will have low loot priority.
+													 <p>Almost all loot from progression and farm raids will be distributed using a <b>loot council.</b> Each week, two randomly assigned officers + raiders will make up the council and loot is allocated once a fair, unanimous decision is made. Socials, trials and new raiders (those who've been promoted to raid in the past 2+ weeks) will have low loot priority.
 Socials must have no expectation of being invited to raid, although if circumstances allow, Thursday raid nights may occasionally see socials.</p>
 <p> Newly recruited members will automatically become a trial and will become so for as long as neccessery for the officers to reach a unanimous conclusion based on their performance - they will either be asked to leave, become a reserve, or to become a member of the raid team.
-If you believe that a player is not up to standards with mechanical awareness, DPS/HPS etc., any issues regarding gameplay and administration (such as raid times, loot competition), please contact Fen (Joicy). The issue will be communicated to the other officers; we will look into this and we will handle issues seriously and in a discrete manner. Be convinced that if what you have said is true and of significance, we will do something about it.</p>
+If you believe that a player is not up to standards with mechanical awareness, DPS/HPS etc., any issues regarding gameplay and administration (such as raid times, loot competition), <b>please contact Fen (Joicy).</b> The issue will be communicated to the other officers; we will look into this and we will handle issues seriously and in a discrete manner. Be convinced that if what you have said is true and of significance, we will do something about it.</p>
 
 					</section>
 
